4d4f9c255c3f4be0d4c655df456da92a6aa40077
Sdet_Ahmet <parisstar033@gmail.com>
2020-12-09 20:51:34 +0100

    conflict cozuldu

--- src/test/java/gmibank/pages/US_06_07_Page.java
+++ src/test/java/gmibank/pages/US_06_07_Page.java
@@ -12,46 +12,46 @@ public class  US_06_07_Page{
         PageFactory.initElements(Driver.getDriver(),this);
 
     }
-//1
+
     @FindBy (id = "account-menu")
     public WebElement accountMenu;
-//2
+
     @FindBy (id = "login-item")
     public WebElement signInPrimary;
-//3
+
     @FindBy (id = "username")
     public WebElement userName;
-//4
+
     @FindBy (name = "password")
     public WebElement passWord;
-//5
+
     @FindBy (xpath = "//*[@class='btn btn-primary']")
     public WebElement signInButton;
-//6
+
     @FindBy (css = "#account-menu > a > span")
     public WebElement clickOnUsername;
-//7
+
     @FindBy (xpath = "(//*[@class='dropdown-item'])[3]")
     public WebElement userInfoClick;
-//8
+
     @FindBy (id = "firstName")
     public WebElement firstNameBoxClick;
-//9
+
     @FindBy (id = "lastName")
     public WebElement lastNameBoxClick;
-//10
+
     @FindBy (id = "email")
     public WebElement emailBoxClick;
-//11
+
     @FindBy (id = "langKey")
     public WebElement languageSelect;
-//12
+
     @FindBy (xpath = "//*[@class='btn btn-primary']")
     public WebElement saveButtonClick;
-//13
+
     @FindBy(xpath="//div[@role='alert']//span//strong")
     public WebElement settingsSaveMessage;
-//14
+
     @FindBy(xpath="//div[@class='invalid-feedback']")
     public WebElement invalidFeedback;
 
