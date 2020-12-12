309547e8e113a84488bfd016b5bc29322143da99
mk2019dnz <mk2019dnz@gmail.com>
2020-12-12 22:05:27 +0300

    "12.12"

--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
         glue = "gmibank/stepdefinitions",
-        tags = "@infoUpdate",
+        tags = "@US09_createANewCustomer",
         dryRun = false
 
 )
--- src/test/java/gmibank/stepdefinitions/Hooks.java
+++ src/test/java/gmibank/stepdefinitions/Hooks.java
@@ -12,19 +12,11 @@ public class Hooks {
     //@Before TestNG deki @BeforeMethod gibidir
     //GLOBAL Hooks annotation
     //her scenario dan once calisir
-    @Before(order = 1)
+    @Before
     public void setUp(){
         //System.out.println("Hooks class- Setup Method");
     }
 
-    //TAGGED Hooks Annotation
-    //Her ozel scenario dan once calisir
- //   @Before(value = "iphone",order = 2)
- //   public void searchIphone(){
-        //System.out.println("Hooks Class - IPHONE method");
-  //  }
-
-    //Her scenario dan sonra
    @After
     public void tearDown(Scenario scenario){
         //System.out.println("Hooks class - tearDown Method ");
