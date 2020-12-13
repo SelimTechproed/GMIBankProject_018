03665418f77c14ef07deab1128761f621d3f8033
Sdet_Ahmet <parisstar033@gmail.com>
2020-12-13 20:00:38 +0100

    commit branch ahmet

--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -12,8 +12,8 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
         glue = "gmibank/stepdefinitions",
-        tags = "@US09_createANewCustomer",
-        dryRun = false
+        tags = "@createnewapplicant",
+        dryRun = true
 
 )
 public class Runner {
