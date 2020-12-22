520bb1734e3312aa690a9c69aeb40f6fd887b0ee
Sdet_Ahmet <parisstar033@gmail.com>
2020-12-22 15:07:59 +0100

    commit branch ahmet

--- src/test/java/gmibank/com/stepdefinitions/US_26_StepDefinitions.java
+++ src/test/java/gmibank/com/stepdefinitions/US_26_StepDefinitions.java
@@ -1,19 +1,15 @@
 package gmibank.com.stepdefinitions;
 
-import gmibank.pojos.*;
-import gmibank.utilities.ConfigurationReader;
+import gmibank.com.pojos.*;
+import gmibank.com.utilities.ConfigurationReader;
 import io.restassured.http.ContentType;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import io.restassured.response.Response.*;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.codehaus.jackson.map.ObjectMapper;
-
 import java.io.IOException;
 import java.util.HashMap;
-
-
+import java.util.Map;
 import static io.restassured.RestAssured.given;
 
 public class US_26_StepDefinitions {
@@ -41,7 +37,7 @@ public class US_26_StepDefinitions {
                 statusCode(200).
                 extract().
                 response();
-      // response.prettyPrint();
+    //   response.prettyPrint();
 
     }
 
@@ -55,17 +51,40 @@ public class US_26_StepDefinitions {
          System.out.println(countries[i].getName());
 
      }
-        System.out.println("country sayisi : " + countries.length);
+
     }
 
     @Then("find out how many countries and verify that there are number country")
     public void find_out_how_many_countries_and_verify_that_there_are_number_country() {
+        System.out.println("country sayisi : " + countries.length);
+
 
     }
 
     @Given("user updates a country using api end point {string}  {string} and its extension {string}")
-    public void user_updates_a_country_using_api_end_point_and_its_extension(String string, String string2, String string3) {
+    public void user_updates_a_country_using_api_end_point_and_its_extension(String endPoint, String replaceContry, String idCountry) {
 
+        Map<String, Object> putBody = new HashMap<>();
+
+        putBody.put("id", 11133);
+        putBody.put("name", replaceContry );
+        putBody.put("states",null);
+
+        Response responsePut = given().
+                contentType(ContentType.JSON).
+                auth().oauth2(ConfigurationReader.getProperty("token")).
+                body(putBody).
+                when().
+                put(endPoint);
+
+        responsePut.prettyPrint();
+
+        responsePut.
+                then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON);
     }
+
 
 }
--- src/test/java/gmibank/com/stepdefinitions/US_29_StepDefinitions.java
+++ src/test/java/gmibank/com/stepdefinitions/US_29_StepDefinitions.java
@@ -1,8 +1,8 @@
 package gmibank.com.stepdefinitions;
 
-import gmibank.utilities.ConfigurationReader;
+
 
 public class US_29_StepDefinitions {
 
-    String userDataQuery ="SELECT * FROM " + ConfigurationReader.getProperty()
+
 }
