22cb6d4a39133a643390cac2497aa8b0ff0023aa
yasen911 <yasenarslan@mail.com>
2020-12-22 11:50:50 +0100

    update

---
+++ target/default-cucumber-reports/report.js
@@ -0,0 +1,564 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/US_12.feature");
+formatter.feature({
+  "name": "Gmi Bank",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@US_12"
+    }
+  ]
+});
+formatter.background({
+  "name": "Gmi Bank Startseite",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer geht zu \"https://gmibank.com/\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_geht_zu(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer sendet einen gültigen Benutzernamen \"team18_employee\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_sendet_einen_gültigen_Benutzernamen(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer sendet ein gültiges Passwort \"Team18employee\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_sendet_ein_gültiges_Passwort(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer klickt auf die login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_klickt_auf_die_login_button()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer klickt auf My Operations",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_klickt_auf_My_Operations()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer klickt auf Button Manage Custormes",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_klickt_auf_Button_Manage_Custormes()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.scenario({
+  "name": "\"First Name Last Name Middle..\" im Model Costumers anzeigt werden soll",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@US_12"
+    },
+    {
+      "name": "@US_12-TC_01"
+    }
+  ]
+});
+formatter.step({
+  "name": "Benutzer muss Id sehen",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_Id_sehen()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer muss First Name sehen",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_First_Name_sehen()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer muss Last Name sehen",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_Last_Name_sehen()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer muss Middle Initial sehen",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_Middle_Initial_sehen()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer muss Email sehen",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_Email_sehen()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer muss MobilePhone Number sehen",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_MobilePhone_Number_sehen()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer muss Phone Number sehen",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_Phone_Number_sehen()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer soll Adress sehen",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_soll_Address_sehen()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer muss Create Date sehen",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_Create_Date_sehen()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.background({
+  "name": "Gmi Bank Startseite",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer geht zu \"https://gmibank.com/\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_geht_zu(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer sendet einen gültigen Benutzernamen \"team18_employee\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_sendet_einen_gültigen_Benutzernamen(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer sendet ein gültiges Passwort \"Team18employee\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_sendet_ein_gültiges_Passwort(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer klickt auf die login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_klickt_auf_die_login_button()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer klickt auf My Operations",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_klickt_auf_My_Operations()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer klickt auf Button Manage Custormes",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_klickt_auf_Button_Manage_Custormes()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.scenario({
+  "name": "\"Die Button view,Edit,Delete sollten anklickbar und auf der Kundenseite sichtbar sein",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@US_12"
+    },
+    {
+      "name": "@US_12-TC_02"
+    }
+  ]
+});
+formatter.step({
+  "name": "Benutzer sieht und klickt auf die View",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_sieht_und_klickt_auf_die_View()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer sieht und klickt auf die Edit",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_sieht_und_klickt_auf_die_Edit()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer sieht und klickt auf die Delete",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_sieht_und_klickt_auf_die_Delete()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.background({
+  "name": "Gmi Bank Startseite",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer geht zu \"https://gmibank.com/\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_geht_zu(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer sendet einen gültigen Benutzernamen \"team18_employee\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_sendet_einen_gültigen_Benutzernamen(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer sendet ein gültiges Passwort \"Team18employee\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_sendet_ein_gültiges_Passwort(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer klickt auf die login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_klickt_auf_die_login_button()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer klickt auf My Operations",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_klickt_auf_My_Operations()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer klickt auf Button Manage Custormes",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_klickt_auf_Button_Manage_Custormes()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.scenario({
+  "name": "Die Button View Bearbeiten geben, über die alle Kundeninformationen ausgefüllt werden können",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@US_12"
+    },
+    {
+      "name": "@US_12-TC_03"
+    }
+  ]
+});
+formatter.step({
+  "name": "Benutzer sieht und klickt auf die View",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_sieht_und_klickt_auf_die_View()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer sieht und klickt auf die Button Edit",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_sieht_und_klickt_auf_die_Button_Edit()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.background({
+  "name": "Gmi Bank Startseite",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer geht zu \"https://gmibank.com/\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_geht_zu(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer sendet einen gültigen Benutzernamen \"team18_employee\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_sendet_einen_gültigen_Benutzernamen(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer sendet ein gültiges Passwort \"Team18employee\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_sendet_ein_gültiges_Passwort(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer klickt auf die login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_klickt_auf_die_login_button()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer klickt auf My Operations",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_klickt_auf_My_Operations()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer klickt auf Button Manage Custormes",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_klickt_auf_Button_Manage_Custormes()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.scenario({
+  "name": "The Edit portal can allow user to create or update the user info",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@US_12"
+    },
+    {
+      "name": "@US_12-TC_04"
+    }
+  ]
+});
+formatter.step({
+  "name": "TC_04 Benutzer sieht und klickt auf die Edit",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Benutzer muss First Name geben",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_First_Name_geben()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer muss Last Name geben",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_Last_Name_geben()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer muss Middle Initial geben",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_Middle_Initial_geben()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer muss Email geben",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_Email_geben()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer muss MobilePhone Number geben",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_MobilePhone_Number_geben()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer muss Phone Number geben",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_Phone_Number_geben()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer soll Adress geben",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_soll_Adress_geben()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Benutzer muss Create Date geben",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "gmibank.stepdefinitions.US_12_StepDefinitions.benutzer_muss_Create_Date_geben()"
+});
+formatter.result({
+  "status": "skipped"
+});
+});
---
+++ target/test-classes/features/US_12.feature
@@ -0,0 +1,54 @@
+@US_12
+Feature: Gmi Bank
+
+  Background: Gmi Bank Startseite
+    Given Benutzer geht zu "https://gmibank.com/"
+    And Benutzer sendet einen gültigen Benutzernamen "team18_employee"
+    And Benutzer sendet ein gültiges Passwort "Team18employee"
+    And Benutzer klickt auf die login button
+    And Benutzer klickt auf My Operations
+    And Benutzer klickt auf Button Manage Custormes
+
+
+  @US_12-TC_01
+  Scenario:"First Name Last Name Middle.." im Model Costumers anzeigt werden soll
+    Given Benutzer muss Id sehen
+    And Benutzer muss First Name sehen
+    And Benutzer muss Last Name sehen
+    And Benutzer muss Middle Initial sehen
+    And Benutzer muss Email sehen
+    And Benutzer muss MobilePhone Number sehen
+    And Benutzer muss Phone Number sehen
+    And Benutzer soll Adress sehen
+    Then Benutzer muss Create Date sehen
+
+    @US_12-TC_02
+  Scenario:"Die Button view,Edit,Delete sollten anklickbar und auf der Kundenseite sichtbar sein
+    Given Benutzer sieht und klickt auf die View
+      And Benutzer sieht und klickt auf die Edit
+      Then Benutzer sieht und klickt auf die Delete
+
+
+    @US_12-TC_03
+    Scenario: Die Button View Bearbeiten geben, über die alle Kundeninformationen ausgefüllt werden können
+     Given Benutzer sieht und klickt auf die View
+      And Benutzer sieht und klickt auf die Button Edit
+
+   @US_12-TC_04
+    Scenario: The Edit portal can allow user to create or update the user info
+     Given TC_04 Benutzer sieht und klickt auf die Edit
+     And Benutzer muss First Name geben
+     And Benutzer muss Last Name geben
+     And Benutzer muss Middle Initial geben
+     And Benutzer muss Email geben
+     And Benutzer muss MobilePhone Number geben
+     And Benutzer muss Phone Number geben
+     And Benutzer soll Adress geben
+     Then Benutzer muss Create Date geben
+
+
+
+
+
+
+
